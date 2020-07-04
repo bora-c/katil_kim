3052812135a7467f68db76d2579bb796aa258c35
Bora Ücelehan <boraboracebi@gmail.com>
2020-07-04 16:33:15 +0300

    server ve client v1

---
+++ client/Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        const int PORT_NO = 6969;
+        const string SERVER_IP = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            //---data to send to the server---
+            string textToSend = Console.ReadLine();
+
+            //---create a TCPClient object at the IP and port no.---
+            TcpClient client = new TcpClient(SERVER_IP, PORT_NO);
+            NetworkStream nwStream = client.GetStream();
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+
+            //---send the text---
+            Console.WriteLine("Sending : " + textToSend);
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+            //---read back the text---
+            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+            int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+            Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+            Console.ReadLine();
+            client.Close();
+        }
+    }
+}
