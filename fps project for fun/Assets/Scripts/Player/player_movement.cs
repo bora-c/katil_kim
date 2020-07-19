57c39f9c92cc764a92a4134321f95b2c2ba977ba
Bora Ücelehan <boraboracebi@gmail.com>
2020-07-19 21:12:42 +0300

    Unity Dosyalar

---
+++ fps project for fun/Assets/Scripts/Player/player_movement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public float speed = 1;
+    public CharacterController player;
+
+    public void Move(){
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        player.transform.position += movement * speed * Time.deltaTime;
+    }
+    private void Update() {
+        Move();
+    }
+}
